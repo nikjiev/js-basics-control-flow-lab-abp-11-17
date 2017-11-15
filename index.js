ee22886e532de7c91a38e694bc76bf90eba6b7dd
Nik Jiev <n.arabadjiev@gmail.com>
2017-11-15 05:57:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,5 +10,5 @@ function scuberGreetingForFeet(distance) {
 }
 
 function ternaryCheckCity(city) {
-  return (city === "NYC") ? "Ok, sounds good" : "No, go."
+  return (city === "NYC") ? "Ok, sounds good." : "No, go."
 }
